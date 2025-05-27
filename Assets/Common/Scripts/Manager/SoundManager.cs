4ec5ac6134547f87983b5e747670ae0e12443203
gain404 <gain17352@gmail.com>
2025-05-27 19:11:55 +0900

    feat : 오디오믹서 추가 & 씬에 오브젝트들 추가

--- Assets/Common/Scripts/Manager/SoundManager.cs
+++ Assets/Common/Scripts/Manager/SoundManager.cs
@@ -1,18 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public static SoundManager instance;
+    
+    //오디오 볼륨 조절
+    //오디오 뮤트 여부
+    //루프 여부
+    //슬라이더 스크립트는 따로 만들어서 붙이기
 }
