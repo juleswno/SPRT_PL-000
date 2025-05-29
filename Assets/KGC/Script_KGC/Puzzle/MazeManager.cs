708c1cb54cfc671db1d698f9a902904860dd9546
KimgiChan07 <wekorean0727@naver.com>
2025-05-29 15:21:47 +0900

    feat: 몬스터AI 맵 동적 베이킹 기능 구현

--- Assets/KGC/Script_KGC/Puzzle/MazeManager.cs
+++ Assets/KGC/Script_KGC/Puzzle/MazeManager.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections;
+using Unity.AI.Navigation;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -6,15 +8,24 @@ public class MazeManager : MonoBehaviour
 {
    [SerializeField] private MazePatternData[] mazeData;
    
+   [Header("NavMesh 베이크를 위한 Surface")]
+   [SerializeField] private NavMeshSurface navMeshSurface;
+   
    private GameObject curMaze;
 
    private void Start()
    {
-      SpawnRandomMaze();
+      StartCoroutine(SpawnRandomMazeAndBake());
    }
 
-   public void SpawnRandomMaze()
+   private IEnumerator SpawnRandomMazeAndBake()
    {
+
+      if (curMaze != null)
+      {
+         Destroy(curMaze);
+      }
+      
       int random = Random.Range(0, mazeData.Length);
       
       MazePatternData selected = mazeData[random];
@@ -23,5 +34,17 @@ public class MazeManager : MonoBehaviour
       {
          curMaze = Instantiate(selected.mazePatternPrefab,Vector3.zero, Quaternion.identity);
       }
+      yield return null;
+
+      if(navMeshSurface != null)
+      {
+         navMeshSurface.BuildNavMesh();
+      }
+      else
+      {
+         Debug.Log("no nav mesh");
+      }
+         
+      
    }
 }
