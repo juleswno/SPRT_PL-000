0dcf59fc6b08ff7a023d7b238f8036acdf0a9cbd
이빛나 <dlql1306@naver.com>
2025-05-29 14:01:12 +0900

    저장
    저장

--- Assets/LBN/Ball.cs
+++ Assets/LBN/Ball.cs
@@ -1,18 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject ballPrefab;       // 생성할 공 프리팹
+    public int numberOfBalls = 10;       // 생성할 공 개수
+    public Vector3 startPosition = Vector3.zero;  // 시작 위치
+    public float spacing = 2.0f;        // 공 간의 간격
+
     void Start()
     {
-        
+        SpawnBalls();
     }
 
-    // Update is called once per frame
-    void Update()
+    void SpawnBalls()
     {
-        
+        for (int i = 0; i < numberOfBalls; i++)
+        {
+            Vector3 spawnPosition = startPosition + new Vector3(i * spacing, 0f, 0f);
+            Instantiate(ballPrefab, spawnPosition, Quaternion.identity);
+        }
     }
 }
+
